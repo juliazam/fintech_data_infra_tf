2bf318aea883252c3a5ee0bdce64cb4bfd893d88
juliazam <juliz@mail.ru>
2026-07-20 15:39:38 +0800

    feat: configure aws provider to use MiniStack endpoints

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
-    required_version = ">= 1.12.0"
+  required_version = ">= 1.12.0"
 
-    required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
@@ -10,7 +10,15 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
-    access_key = "test"
-    secret_key = "test"
+  region = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    iam = "http://localhost:4566"
+  }
+  
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
 }
