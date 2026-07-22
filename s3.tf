86bfd76cddc8cc7f696e70c9cc7367ae0256a356
juliazam <juliz@mail.ru>
2026-07-22 21:59:55 +0800

    refactor: extract S3 bucket into reusable module

--- s3.tf
+++ s3.tf
@@ -1,7 +1,8 @@
-resource "aws_s3_bucket" "raw_data" {
-  bucket = "${local.full_name}-raw-data"
-}
-
 resource "aws_s3_bucket" "state_bucket" {
   bucket = "${local.full_name}-tfstate"
+}
+
+module "raw_data" {
+  source = "./modules/s3"
+  bucket_name = "${local.full_name}-raw-data"
 }
