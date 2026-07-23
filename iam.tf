a4e4438406361217da3760e0623e8a023ae30ccf
juliazam <juliz@mail.ru>
2026-07-23 09:57:31 +0800

    feat: add IAM role and policy for Lambda S3 access

---
+++ iam.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "${local.full_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_s3_access" {
+  name = "${local.full_name}-lambda-s3-access"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject"]
+        Resource = "${module.raw_data.bucket_arn}/*"
+      }
+    ]
+  })
+}
